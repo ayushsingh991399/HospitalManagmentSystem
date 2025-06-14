9167afd60e10aa0d94a675917beccdc1df20d335
Ayush singh <163518600+ayushsingh991399@users.noreply.github.com>
2025-06-14 13:54:20 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
-          <p className=' text-gray-500'>54709 Rohta road <br /> Meerut, Uttar Pradesh, India</p>
+          <p className=' text-gray-500'>250002 Rohta road <br /> Meerut, Uttar Pradesh, India</p>
           <p className=' text-gray-500'>Tel: (91) 7037942836 <br /> Email: ayushkumar991399@gmail.com</p>
           <p className=' font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
